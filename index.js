8a2c57515209cd97d082bf14c0806722de8b9477
Sycoms <77230943+Benyamin35@users.noreply.github.com>
2022-09-30 23:31:49 +0400

    lox

--- index.js
+++ index.js
@@ -2,39 +2,61 @@ let todo = document.querySelector(".todo");
 let link = document.querySelector(".link");
 let input = document.querySelector('input');
 
-
-
-
-function getInputValue (){
-    let inputValue = input.value;
-    inputValue = " ";
-    //clone = todoMain.cloneNode(true)
-    //todo.appendChild(clone);
-    createElements()
-    
-}
-
-function createElements () {
+function createTodos () {
     let todoMain = document.createElement("div");
-    todoMain.style.backgroundColor = "yellow";    
     todoMain.classList.add("todoMain");
     let todoInput = document.createElement("input");
         todoInput.type = "checkbox";
         todoInput.classList.add("todoInput");
     let todoContent = document.createElement("div")
         todoContent.classList.add("todoContent");
-    let todoText = document.createElement("span");
+    let todoText = document.createElement("input");
         todoText.classList.add("todoText"); 
-        todoText.innerHTML = input.value;
-
+        todoText.setAttribute('readonly', 'true')
+        todoText.type = "text"
+        todoText.value = input.value;
+        input.value = ' ';
+    let btns = document.createElement("div");
+        btns.classList.add("btns");
+    let edit = document.createElement("button");
+        edit.classList.add("edit");
+        edit.innerHTML = "Edit";
+    let remove = document.createElement("button");
+        remove.classList.add("remove");
+        remove.innerHTML = "Remove";
     
-    todo.appendChild(todoMain)
-    todoMain.appendChild(todoInput)
-    todoMain.appendChild(todoContent)
-    todoContent.appendChild(todoText)
-    
+
+    edit.addEventListener('click', (e) => {
+        if(edit.innerHTML == "Edit") {
+            edit.innerHTML = "Save"
+            todoText.removeAttribute('readonly', 'true');
+
+        }else{
+            edit.innerText = "Edit";
+			todoText.setAttribute('readonly', 'true')
+            todoText.type = "text"
+        }
+
+       
+    })
+    remove.addEventListener('click', (e) =>{
+        todo.removeChild(todoMain)
+    });
 
 
-    
-}
+    todo.appendChild(todoMain);
+    todoMain.appendChild(todoInput);
+    todoMain.appendChild(todoContent);
+    todoContent.appendChild(todoText);
+    todoContent.appendChild(btns)
+    btns.appendChild(edit)
+    btns.appendChild(remove)
+
+};
+
+
+
+
+
+
 
